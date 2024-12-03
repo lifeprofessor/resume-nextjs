a91ed4506b2217f12d78af94a129f56a62637265
LimSeongMin <lifeprof@naver.com>
2024-12-03 13:30:17 +0900

    footer에 회사 로고 삽입

--- component/footer/index.tsx
+++ component/footer/index.tsx
@@ -1,26 +1,26 @@
 import { Col, Row } from 'reactstrap';
-import { PropsWithChildren } from 'react';
-import { EmptyRowCol, HrefTargetBlank } from '../common';
+import { EmptyRowCol } from '../common';
 
-import { IFooter } from './IFooter';
 import { Style } from '../common/Style';
+
+import companyLogo from '../../asset/companyLogo.png';
 
 export const Footer = {
   Component,
 };
 
-function Component({ payload }: PropsWithChildren<{ payload: IFooter.Payload }>) {
+function Component() {
   return (
     <Row>
       <Col style={Style.footerCover}>
         <div style={Style.footer} className="text-center mt-4">
           <EmptyRowCol>
             <small>
-              v.{`${payload.version} / `}
-              {/* Github 주소는 origin repository 의 주소를 넣는다. */}
-              <HrefTargetBlank url="https://github.com/uyu423/resume-nextjs" text="Github" />
-              {' / '}
-              Thanks for <HrefTargetBlank url="https://blog.outsider.ne.kr/1234" text="Outsider" />
+              <img
+                src={companyLogo}
+                alt="인생교수의 AI 연구소"
+                style={{ width: '250px', height: 'auto' }}
+              />
             </small>
           </EmptyRowCol>
         </div>
